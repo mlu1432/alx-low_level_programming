ed0e437d8d2d35a4d869105252516ca99922aa4c
mlu1432 <luxturesekwati@gmail.com>
2023-02-23 01:03:31 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 
 /**
-<<<<<<< HEAD
 * print_to_98 - prints all natural numbers from n to 98,
 * @n: print from this number
-* Return: Always 0.
+* Return: Alwalys 0.
 */
 void print_to_98(int n)
 {
@@ -31,37 +30,4 @@ else
 printf("%d\n", j);
 }
 }
-=======
- * print_to_98 - Prints all natural numbers from n to 98, followed by a new line.
- * @n: The number to start counting from.
- * Return: Always 0.
- */
-void print_to_98(int n)
-{
-
-    if (n <= 98)
-    {
-    for (i = n; i < 98; i++)
-    {
-    _putchar(i / 10 + '0');
-    _putchar(i % 10 + '0');
-    _putchar(',');
-    _putchar(' ');
-    }
-    }
-    else
-    {
-    for (i = n; i > 98; i--)
-    {
-    _putchar(i / 10 + '0');
-    _putchar(i % 10 + '0');
-    _putchar(',');
-    _putchar(' ');
-    }
-    }
-
-    _putchar('9');
-    _putchar('8');
-    _putchar('\n');
->>>>>>> c4e4baaf5212d75be151722e9ac02410ce3aec70
 }
