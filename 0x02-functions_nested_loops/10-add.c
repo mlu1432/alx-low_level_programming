909370213b6330539de96fc1a23ebca77e12b977
mlu1432 <luxturesekwati@gmail.com>
2023-02-22 22:30:44 -0800

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,38 +1,13 @@
 #include "main.h"
 
 /**
- * add - adds two integers and returns the result
- * @a: the first integer
- * @b: the second integer
- *
- * Return: the sum of a and b
+ * add - adds tow values
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
+ * REturn: Integer value
  */
+int add(int n1, int n2)
 
-int add(int a, int b)
 {
-   int sum = a + b;
-   int result = 0;
-   int digit = 0;
-   if (sum < 0)
-   {
-       _putchar('-');
-       sum *= -1;
-   }
-
-   result = sum;
-
-   do {
-       digit = result % 10;
-       _putchar(digit + '0');
-       result /= 10;
-   } while (result != 0);
-
-   if (sum == 0)
-   {
-       _putchar('0');
-   }
-
-   _putchar('\n');
-
-   return sum;
+	return (n1 + n2);
 }
