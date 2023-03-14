f27653c08d2007cee15d2533599ea65f1da33b52
mlu1432 <luxturesekwati@gmail.com>
2023-03-14 09:02:20 -0700

    updated

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,8 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
