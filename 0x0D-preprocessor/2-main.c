29113437657f0bb655df9792c50a31d79b334810
mlu1432 <luxturesekwati@gmail.com>
2023-04-01 12:11:00 -0700

    Fix modified files

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(void) {
-    printf("Compiled from file: %s\n", __FILE__);
-    return 0;
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int ain(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
 }
