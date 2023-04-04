b2b1a9f5a5184f41baaa58a3e811dcb7c8b6b819
mlu1432 <luxturesekwati@gmail.com>
2023-04-03 21:43:57 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 0
-
-section .text
-    global main
-    extern printf
-
+global   main
+	  extern    printf
 main:
-    push rbp
-    mov rbp, rsp
-
-    lea rdi, [hello]
-    xor eax, eax
-    call printf
-
-    mov eax, 0
-    pop rbp
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
