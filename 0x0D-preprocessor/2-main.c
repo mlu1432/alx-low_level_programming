20a3b2b435e3357ffa2730008c1a7d456b10c2ba
Lucas Sekwati <luxturesekwati@gmail.com>
2023-06-19 19:52:55 -0700

    Preprocessor-update

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The program's entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("File compiling: %s\n", __FILE__);
+
+	return (0);
+}
