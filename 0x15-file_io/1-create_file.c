c6f55483808b96fef5beaffdc5a0b7d08fb09f36
Lucas Sekwati <Luxturesekwati@gmail.com>
2023-07-28 15:04:58 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,19 +10,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, length = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[len];)
-			length++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, length);
+	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
 		return (-1);
